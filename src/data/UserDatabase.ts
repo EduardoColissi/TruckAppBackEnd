2be50c3094bc5b03a59ed696efa45e01f9a72264
EduardoColissi <educolissi@hotmail.com>
2022-11-23 16:03:16 -0300

    TRUCK-20 Mudado table name para users

--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -4,7 +4,7 @@ import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
 
-  private static TABLE_NAME = "users_web";
+  private static TABLE_NAME = "users";
 
   public findUserByUserName = async (userName: string) => {
     try {
