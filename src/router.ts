924ef13789302766ae5b437e32a79392a2b25e69
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2023-01-02 14:30:33 -0300

    TRUCK-52 mudanças não signficativas nas rotas

--- src/router.ts
+++ src/router.ts
@@ -13,12 +13,13 @@ routes.post("/users/signup", userFormValidator, user.create);
 routes.post("/users/login", user.login);
 routes.get("/users/all", user.getAllUsers);
 routes.get("/users/all/user/:id", user.getUserById);
-routes.put("/users/userUpdate/:id", user.update);
-routes.delete("/deleteUser/:id", user.delete);
+routes.put("/users/update/:id", user.update);
+routes.delete("/users/delete/:id", user.delete);
 routes.get("/users/search", user.search);
 
 routes.get("/freights/all/", freights.getAllFreights);
 routes.post("/freights/create/", freights.create);
+routes.get("/freights/all/freight/:id", freights.getFreightsById);
 routes.put("/freights/update/:id", freights.updateFreight);
 routes.delete("/freights/delete/:id", freights.delete);
 routes.get("/freights/search", freights.search);
